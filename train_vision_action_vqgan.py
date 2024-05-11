a3ddb0e1893eb3df68f46b28300a66bf3521566d
root <root@e1287c01561d4069b83a1faa332c84d800000L.1yhk1dhplj4uxeqzcfdtgxiqqa.jx.internal.cloudapp.net>
2024-05-11 04:19:50 +0000

    upate

--- train_vision_action_vqgan.py
+++ train_vision_action_vqgan.py
@@ -30,7 +30,7 @@ def main():
     parser.add_argument('--embedding_dim', type=int, default=256)
     parser.add_argument('--n_codes', type=int, default=16384)
     parser.add_argument('--n_hiddens', type=int, default=32)
-    parser.add_argument('--lr', type=float, default=3e-5)
+    parser.add_argument('--lr', type=float, default=5e-6)
     parser.add_argument('--downsample', nargs='+', type=int, default=(2, 16, 16))
     parser.add_argument('--disc_channels', type=int, default=64)
     parser.add_argument('--disc_layers', type=int, default=3)
