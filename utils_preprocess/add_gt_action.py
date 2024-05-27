0424f3edfb90ec0bba358238857d2170c1e52e1f
Rundong Luo <rundong luo2002@gmail.com>
2024-05-27 05:03:11 +0000

    update

--- utils_preprocess/add_gt_action.py
+++ utils_preprocess/add_gt_action.py
@@ -1,5 +1,6 @@
 import os
 import json
+from tqdm import tqdm
 
 src_root = '/mnt/data-rundong/bridge2_processed/tokenized/test/'
 formatted_file = './bridge2_processed/formatted.jsonl'
@@ -22,11 +23,12 @@ num_shards = 8
 for num_shard in range(num_shards):
     f_dst = open(os.path.join(dst_root, f'{num_shard}_stacked.jsonl'), 'w')
     file_path = os.path.join(src_root, f'{num_shard}_stacked.jsonl')
-    with open(file_path, 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            instance_data = json.loads(line)
-            trajectory_id = instance_data['trajectory_id']
-            view = instance_data['view']
-            start_frame = instance_data['start_frame']
-            gt_actions = formatted_data[(trajectory_id, view)][start_frame+5:start_frame+11] if start_frame != -1 else [[0. for _ in range(7)]] + formatted_data[(trajectory_id, view)][0:5]
+    lines = open(file_path, 'r').readlines()
+    for line in tqdm(lines):
+        instance_data = json.loads(line)
+        trajectory_id = instance_data['trajectory_id']
+        view = instance_data['view']
+        start_frame = instance_data['start_frame']
+        gt_actions = formatted_data[(trajectory_id, view)][start_frame+5:start_frame+11] if start_frame != -1 else [[0. for _ in range(7)]] + formatted_data[(trajectory_id, view)][0:5]
+        instance_data['gt_actions'] = gt_actions
+        f_dst.write(json.dumps(instance_data) + '\n')
