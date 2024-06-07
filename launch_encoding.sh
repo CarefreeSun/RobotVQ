dd430edb33f399b2dccd5a8b00313e868271e10c
Rundong Luo <rundong luo2002@gmail.com>
2024-06-07 03:01:03 +0000

    update

--- launch_encoding.sh
+++ launch_encoding.sh
@@ -1,28 +1,11 @@
-for i in {0..7}
+for shard_cnt in $(seq 0 5) 
 do
-    python encode.py --gpu_id $i --split 'train' --normalize --wo_transformer_residual \
-    --num_shards 96 --start_shard 0 \
-    --n_stacked_clip 1 \
-    --weight_path '/mnt/data-rundong/VQ3D-vision-action/0602-action111-bridgeRT-noMask-woResidual-continue0531/checkpoints/step_checkpoint-step_50000.ckpt' & 
-done
-for i in {0..7}
-do
-    python encode.py --gpu_id $i --split 'train' --normalize --wo_transformer_residual \
-    --num_shards 96 --start_shard 8 \
-    --n_stacked_clip 1 \
-    --weight_path '/mnt/data-rundong/VQ3D-vision-action/0602-action111-bridgeRT-noMask-woResidual-continue0531/checkpoints/step_checkpoint-step_50000.ckpt' & 
-done
-for i in {0..7}
-do
-    python encode.py --gpu_id $i --split 'train' --normalize --wo_transformer_residual \
-    --num_shards 96 --start_shard 16 \
-    --n_stacked_clip 1 \
-    --weight_path '/mnt/data-rundong/VQ3D-vision-action/0602-action111-bridgeRT-noMask-woResidual-continue0531/checkpoints/step_checkpoint-step_50000.ckpt' & 
-done
-for i in {0..7}
-do
-    python encode.py --gpu_id $i --split 'train' --normalize --wo_transformer_residual \
-    --num_shards 96 --start_shard 32 \
-    --n_stacked_clip 1 \
-    --weight_path '/mnt/data-rundong/VQ3D-vision-action/0602-action111-bridgeRT-noMask-woResidual-continue0531/checkpoints/step_checkpoint-step_50000.ckpt' & 
+    start_shard=$(((0 + $shard_cnt) * 8))  # Removed unnecessary variable and ensured correct arithmetic
+    for i in $(seq 0 7)
+    do
+        python encode.py --gpu_id $i --split 'train' --normalize --wo_transformer_residual \
+        --num_shards 240 --start_shard $start_shard \
+        --n_stacked_clip 1 \
+        --weight_path '/mnt/data-rundong/VQ3D-vision-action/0602-action111-bridgeRT-noMask-woResidual-continue0531/checkpoints/step_checkpoint-step_50000.ckpt' & 
+    done
 done
