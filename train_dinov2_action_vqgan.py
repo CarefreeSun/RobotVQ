399a9da3279a8df7894fbbd99fd6a38ad5a38046
carefreesun <carefree_sun@stu.pku.edu.cn>
2024-07-12 12:41:36 +0800

    dinov2 encoder

--- train_dinov2_action_vqgan.py
+++ train_dinov2_action_vqgan.py
@@ -64,7 +64,7 @@ def main():
                                 "/mnt/robotdata/RT1/RT1-images"))
     parser.add_argument("--normalize", action="store_true", help="normalize the actions")
     parser.add_argument("--sequence_length", type=int, default=6)
-    parser.add_argument("--batch_size", type=int, default=4)
+    parser.add_argument("--batch_size", type=int, default=2)
     parser.add_argument("--num_workers", type=int, default=1)
     parser.add_argument("--resolution", type=int, default=256)
     parser.add_argument('--image_channels', type=int, default=3)
