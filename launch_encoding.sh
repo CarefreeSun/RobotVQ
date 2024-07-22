ba2d3caf473201751c2e1f553cb902f37c5b1f2d
CarefreeSun <carefree_sun@stu.pku.edu.cn>
2024-07-22 09:44:53 +0800

    Update

--- launch_encoding.sh
+++ launch_encoding.sh
@@ -1,4 +1,4 @@
-for shard_cnt in $(seq 0 5) 
+for shard_cnt in $(seq 0 1) 
 do
     start_shard=$((0 + $shard_cnt*8))  # Removed unnecessary variable and ensured correct arithmetic
     for i in $(seq 0 7)
@@ -6,8 +6,7 @@ do
         python encode.py --gpu_id $i --split 'train' --normalize --wo_transformer_residual \
         --num_shards 240 --start_shard $start_shard \
         --n_stacked_clip 1 \
-        --weight_path '/mnt/data-rundong/VQ3D-vision-action/0715-dinov2-action111-bridge-noMask-woResidual/checkpoints/step_checkpoint-step_15000.ckpt' &
+        --weight_path '/mnt/data-rundong/VQ3D-vision-action/0715-dinov2-action111-bridge-noMask-woResidual/checkpoints/step_checkpoint-step_25000.ckpt' &
     done
 done
-nvidia-smi
-python -c "import time; print("start tokenizing..."); time.sleep(14*24*60*60)"
+python -c "import time; time.sleep(14*24*60*60)"
