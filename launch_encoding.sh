21fa0fcad436c8a7c819b5a2392762004265c993
CarefreeSun <carefree_sun@stu.pku.edu.cn>
2024-08-03 13:01:51 +0800

    Update

--- launch_encoding.sh
+++ launch_encoding.sh
@@ -6,7 +6,7 @@ do
         python encode.py --gpu_id $i --split 'train' --normalize --wo_transformer_residual \
         --num_shards 240 --start_shard $start_shard \
         --n_stacked_clip 1 \
-        --weight_path '/mnt/data-rundong/VQ3D-vision-action/0715-dinov2-action111-bridge-noMask-woResidual/checkpoints/step_checkpoint-step_25000.ckpt' &
+        --weight_path '/mnt/data-rundong/VQ3D-vision-action/0725-dinov2-action111-bridgeRT-noMask-woResidual-usePixelWeight-continueTrain0722/checkpoints/' &
     done
 done
 python -c "import time; time.sleep(14*24*60*60)"
