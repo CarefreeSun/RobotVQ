a2ea5f3ef2fe7bf5d41c4d2ca232d6c2ad8dc8c4
CarefreeSun <carefree_sun@stu.pku.edu.cn>
2024-10-17 12:22:08 +0800

    encode 2dec dropout01 step 8000

--- encode_pizza_width_v2.py
+++ encode_pizza_width_v2.py
@@ -86,7 +86,7 @@ assert args.sequence_length == 6
 
 assert args.normalize and args.wo_transformer_residual
 
-args.dst_dir = '/mnt/data-rundong/robot_datasets/' + args.weight_path.split('/')[-3] + '_step10000' + '_tokenized'
+args.dst_dir = '/mnt/data-rundong/robot_datasets/' + args.weight_path.split('/')[-3] + '_step8000' + '_tokenized'
 
 model = VQFinetuneEval(args)
 state_dict = torch.load(args.weight_path, map_location='cpu')['state_dict']
