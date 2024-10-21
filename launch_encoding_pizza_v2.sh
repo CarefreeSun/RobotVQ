6deeb3636c1296c1fec6830b07dbd178b1fe8a05
CarefreeSun <carefree_sun@stu.pku.edu.cn>
2024-10-21 10:40:06 +0800

    2l drop01 step20000

--- launch_encoding_pizza_v2.sh
+++ launch_encoding_pizza_v2.sh
@@ -16,7 +16,7 @@ do
         python encode_pizza_width_v2.py --gpu_id $i --split 'train' --normalize --wo_transformer_residual \
         --num_shards 128 --start_shard $start_shard \
         --n_stacked_clip 1 \
-        --weight_path '/mnt/data-rundong/VQ3D-vision-action/0929-ftpizza-4ldec-bs128-open0close1-lr1e-6-width07-continue-0725-step95000/checkpoints/step_checkpoint-step_50000.ckpt' &
+        --weight_path 'VQ3D-vision-action/1015-ftpizza-2ldec-bs128-open0close1-lr1e-6-width07-continue-0725-step95000/checkpoints/step_checkpoint-step_20000.ckpt' &
     done
 done
 python -c "import time; time.sleep(1*24*60*60)"
@@ -24,3 +24,4 @@ python -c "import time; time.sleep(1*24*60*60)"
 
 # --weight_path '/mnt/data-rundong/VQ3D-vision-action/0920-finetune-pizza-lr1e-6-bs16-open0close1/checkpoints/step_checkpoint-step_56000.ckpt' &
 # /mnt/data-rundong/VQ3D-vision-action/1015-ftpizza-2ldec-bs128-open0close1-lr1e-6-width07-continue-0725-step95000/checkpoints/step_checkpoint-step_8000.ckpt
+# /mnt/data-rundong/VQ3D-vision-action/0929-ftpizza-4ldec-bs128-open0close1-lr1e-6-width07-continue-0725-step95000/checkpoints/step_checkpoint-step_50000.ckpt
