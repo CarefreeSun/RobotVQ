79a29cb68b3a1790011e4e673f9c9cf8b7de3a85
CarefreeSun <carefree_sun@stu.pku.edu.cn>
2024-10-23 14:46:55 +0800

    train 4l drop01 with crop

--- tats/tats_moreactdeclayer.py
+++ tats/tats_moreactdeclayer.py
@@ -714,28 +714,28 @@ class ActionEncoderStack(nn.Module):
         return torch.cat([encoder(x_) for encoder, x_ in zip(self.encoders, x_split)], dim=-1) # B, T, embed_dim, 7 (sum of input_dims)
 
 # 2-layer action decoder    
-class ActionDecoder(nn.Module):
-    def __init__(self, embed_dim, hidden_dim, output_dim, activation=torch.tanh):
-        super().__init__()
-        self.fc1 = nn.Linear(embed_dim * output_dim, hidden_dim)
-        self.act1 = SiLU()
-        self.dropout1 = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(hidden_dim, output_dim)
-        self.start_block = nn.Sequential(
-            SiLU(),
-            nn.LayerNorm(embed_dim * output_dim)
-        )
-        self.activation = activation
+# class ActionDecoder(nn.Module):
+#     def __init__(self, embed_dim, hidden_dim, output_dim, activation=torch.tanh):
+#         super().__init__()
+#         self.fc1 = nn.Linear(embed_dim * output_dim, hidden_dim)
+#         self.act1 = SiLU()
+#         self.dropout1 = nn.Dropout(0.5)
+#         self.fc2 = nn.Linear(hidden_dim, output_dim)
+#         self.start_block = nn.Sequential(
+#             SiLU(),
+#             nn.LayerNorm(embed_dim * output_dim)
+#         )
+#         self.activation = activation
 
-    def forward(self, x):
-        # x is in shape B, T, embed_dim
-        h = self.start_block(x)
-        h = self.fc1(h)
-        h = self.act1(h)
-        h = self.dropout1(h)
-        h = self.fc2(h)
-        h = self.activation(h)
-        return h
+#     def forward(self, x):
+#         # x is in shape B, T, embed_dim
+#         h = self.start_block(x)
+#         h = self.fc1(h)
+#         h = self.act1(h)
+#         h = self.dropout1(h)
+#         h = self.fc2(h)
+#         h = self.activation(h)
+#         return h
 
 # 3-layer action decoder
 # class ActionDecoder(nn.Module):
@@ -770,42 +770,42 @@ class ActionDecoder(nn.Module):
 #         return h
 
 # 4 layer action decoder
-# class ActionDecoder(nn.Module):
-#     def __init__(self, embed_dim, hidden_dim, output_dim, activation=torch.tanh):
-#         super().__init__()
-#         self.fc1 = nn.Linear(embed_dim * output_dim, hidden_dim * 4)
-#         self.act1 = SiLU()
-#         self.dropout1 = nn.Dropout(0.1)
-#         self.fc2 = nn.Linear(hidden_dim * 4, hidden_dim * 4)
-#         self.act2 = SiLU()
-#         self.dropout2 = nn.Dropout(0.1)
-#         self.fc3 = nn.Linear(hidden_dim * 4, hidden_dim)
-#         self.act3 = SiLU()
-#         self.dropout3 = nn.Dropout(0.1)
-#         self.fc4 = nn. Linear(hidden_dim, output_dim)
-#         self.start_block = nn.Sequential(
-#             SiLU(),
-#             nn.LayerNorm(embed_dim * output_dim)
-#         )
-#         self.activation = activation
+class ActionDecoder(nn.Module):
+    def __init__(self, embed_dim, hidden_dim, output_dim, activation=torch.tanh):
+        super().__init__()
+        self.fc1 = nn.Linear(embed_dim * output_dim, hidden_dim * 4)
+        self.act1 = SiLU()
+        self.dropout1 = nn.Dropout(0.1)
+        self.fc2 = nn.Linear(hidden_dim * 4, hidden_dim * 4)
+        self.act2 = SiLU()
+        self.dropout2 = nn.Dropout(0.1)
+        self.fc3 = nn.Linear(hidden_dim * 4, hidden_dim)
+        self.act3 = SiLU()
+        self.dropout3 = nn.Dropout(0.1)
+        self.fc4 = nn. Linear(hidden_dim, output_dim)
+        self.start_block = nn.Sequential(
+            SiLU(),
+            nn.LayerNorm(embed_dim * output_dim)
+        )
+        self.activation = activation
 
-#     def forward(self, x):
-#         # x is in shape B, T, embed_dim
-#         # for the output, the last entry falls into [0, 1] while other entries are in [-1, 1]
-#         h = self.start_block(x)
-#         h = self.fc1(h)
-#         h = self.act1(h)
-#         h = self.dropout1(h)
-#         h = self.fc2(h)
-#         h = self.act2(h)
-#         h = self.dropout2(h)
-#         h = self.fc3(h)
-#         h = self.act3(h)
-#         h = self.dropout3(h)
-#         h = self.fc4(h)
-#         h = self.activation(h)
-#         # h = torch.cat([torch.tanh(h[:, :-1]), torch.sigmoid(h[:, -1:])], dim=1)
-#         return h
+    def forward(self, x):
+        # x is in shape B, T, embed_dim
+        # for the output, the last entry falls into [0, 1] while other entries are in [-1, 1]
+        h = self.start_block(x)
+        h = self.fc1(h)
+        h = self.act1(h)
+        h = self.dropout1(h)
+        h = self.fc2(h)
+        h = self.act2(h)
+        h = self.dropout2(h)
+        h = self.fc3(h)
+        h = self.act3(h)
+        h = self.dropout3(h)
+        h = self.fc4(h)
+        h = self.activation(h)
+        # h = torch.cat([torch.tanh(h[:, :-1]), torch.sigmoid(h[:, -1:])], dim=1)
+        return h
     
 
 class ActionDecoderStack(nn.Module):
