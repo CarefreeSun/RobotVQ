925ad28533bcab06c3544c4003326fa631004a4b
CarefreeSun <carefree_sun@stu.pku.edu.cn>
2024-10-30 14:54:19 +0800

    actions focusing on gripper

---
+++ launch_encoding_pizza_grip.sh
@@ -0,0 +1,15 @@
+for shard_cnt in $(seq 0 1) 
+do
+    start_shard=$(($1 + $shard_cnt*8))  # Removed unnecessary variable and ensured correct arithmetic
+    for i in $(seq 0 7)
+    do
+        python encode_pizza_grip.py --gpu_id $i --split 'train' --normalize --wo_transformer_residual \
+        --num_shards 16 --start_shard $start_shard \
+        --n_stacked_clip 1 \
+        --weight_path '/mnt/data-rundong/VQ3D-vision-action/1023-ftpizza-crop-4ldec-dropout01-bs128-open0close1-lr1e-6-width07-continue-0725-step95000/checkpoints/step_checkpoint-step_20000.ckpt' &
+    done
+done
+python -c "import time; time.sleep(1*8*60*60)"
+
+
+# --weight_path '/mnt/data-rundong/VQ3D-vision-action/0920-finetune-pizza-lr1e-6-bs16-open0close1/checkpoints/step_checkpoint-step_56000.ckpt' &
