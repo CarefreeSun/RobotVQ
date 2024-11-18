0cb4357d4eb7256f94d205b96a1698940ca16db4
CarefreeSun <carefree_sun@stu.pku.edu.cn>
2024-11-18 11:12:55 +0800

    new version of vq

--- tats/__init__.py
+++ tats/__init__.py
@@ -1,14 +1,14 @@
 # Copyright (c) Meta Platforms, Inc. All Rights Reserved
 
-from .data import VideoData
-from .download import load_transformer, load_vqgan, download
+# from .data import VideoData
+# from .download import load_transformer, load_vqgan, download
 from .dataloader_VLA import get_image_action_dataloader, get_image_action_dataloader_width
-from .tats_vqgan import VQGAN
-from .tats_vision import VQGANVision
-from .tats_vision_action import VQGANVisionAction, VQGANVisionActionEval
-from .tats_dino_action import VQGANDinoV2Action, VQGANDinoV2ActionEval
-from .tats_reg import VQGANDinoV2Action as VQFinetune
-from .tats_reg import VQGANDinoV2ActionEval as VQFinetuneEval
-from .tats_transformer import Net2NetTransformer
+# from .tats_vqgan import VQGAN
+# from .tats_vision import VQGANVision
+# from .tats_vision_action import VQGANVisionAction, VQGANVisionActionEval
+# from .tats_dino_action import VQGANDinoV2Action, VQGANDinoV2ActionEval
+from .joint_vq import VQGANDinoV2Action as VQFinetune
+from .joint_vq import VQGANDinoV2ActionEval as VQFinetuneEval
+# from .tats_transformer import Net2NetTransformer
 from .utils import count_parameters, AverageMeter, CustomCrop
 
