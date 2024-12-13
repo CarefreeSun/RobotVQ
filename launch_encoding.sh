c926b9274f77efb16bbc42f576eb43c3e3dbb327
CarefreeSun <carefree_sun@stu.pku.edu.cn>
2024-12-13 14:03:21 +0800

    update

--- launch_encoding.sh
+++ launch_encoding.sh
@@ -6,7 +6,7 @@ do
         python encode_latest.py --gpu_id $i --split 'train' --normalize --wo_transformer_residual \
         --num_shards 64 --start_shard $start_shard \
         --n_stacked_clip 1 \
-        --weight_path '/mnt/data-rundong/RobotVQ/1210-lr1e-6-8node-8v100-bs4-seqlen3-attn12-7actdec4-drop01-crop-kla-dim1024-imagenetnorm-imagegan-ganfeatloss02/checkpoints/step_checkpoint-step_10000.ckpt' &
+        --weight_path '/mnt/data-rundong/RobotVQ/1210-lr1e-6-8node-8v100-bs4-seqlen3-attn12-7actdec4-drop01-crop-kla-dim1024-imagenetnorm-imagegan-ganfeatloss02/checkpoints/step_checkpoint-step_40000.ckpt' &
     done
 done
 python -c "import time; time.sleep(14*24*60*60)"
