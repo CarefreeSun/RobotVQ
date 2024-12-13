2d336b5037dfa9b524421e80d2ba97f9010174fb
CarefreeSun <carefree_sun@stu.pku.edu.cn>
2024-12-13 16:06:09 +0800

    update

--- encode_latest.py
+++ encode_latest.py
@@ -89,7 +89,7 @@ assert args.normalize and args.wo_transformer_residual
 
 assert args.n_stacked_clips == 1
 
-args.dst_dir = '/mnt/data-rundong/robot_datasets/' + args.weight_path.split('/')[-3] + '_step40000' + '_tokenized'
+args.dst_dir = '/mnt/data-rundong/robot_datasets/' + args.weight_path.split('/')[-3] + '_step50000' + '_tokenized'
 
 model = VQFinetuneEval(args)
 state_dict = torch.load(args.weight_path, map_location='cpu')['state_dict']
